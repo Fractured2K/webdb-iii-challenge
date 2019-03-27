08468b0022616f6e9e8d2037e23a162729094789
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-27 13:24:03 -0700

    Handle create cohort errors

--- routes/cohorts.js
+++ routes/cohorts.js
@@ -6,6 +6,13 @@ const knexConfig = require("../knexfile").development;
 const db = knex(knexConfig);
 
 // Create cohort
-router.post("/", async (req, res) => {});
+router.post("/", async (req, res) => {
+	try {
+	} catch (err) {
+		res.status(500).json({
+			message: "Sorry, but there was an error while creating that cohort"
+		});
+	}
+});
 
 module.exports = router;
