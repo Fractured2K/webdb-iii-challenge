3c965cec93517ec4c85d928e092d02add4f8c177
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-27 13:52:52 -0700

    Handle update cohort endpoint

--- routes/cohorts.js
+++ routes/cohorts.js
@@ -64,6 +64,18 @@ router.get("/:id", async (req, res) => {
 // Update cohort
 router.put("/:id", async (req, res) => {
 	try {
+		const { id } = req.params;
+
+		const cohort = await db("cohorts")
+			.where({ id })
+			.update(req.body);
+
+		if (cohort < 0)
+			return res
+				.status(404)
+				.json({ message: "Sorry, but that cohort doesn't exist " });
+
+		res.status(200).json(cohort);
 	} catch (err) {
 		res.status(500).json({
 			message:
