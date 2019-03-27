94664d31fef07de7296a697e0f4e9adf0a92fca7
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-27 13:22:28 -0700

    Define create cohort endpoint

--- routes/cohorts.js
+++ routes/cohorts.js
@@ -5,4 +5,7 @@ const knex = require("knex");
 const knexConfig = require("../knexfile").development;
 const db = knex(knexConfig);
 
+// Create cohort
+router.post("/", async (req, res) => {});
+
 module.exports = router;
