ac433ba673ed7db55c14847a1b9f6504e3547f1f
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-27 13:27:47 -0700

    Handle create cohort

--- index.js
+++ index.js
@@ -8,12 +8,8 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 
-server.use("/", (req, res) =>
-	res.status(200).json({ message: "Sanity check" })
-);
-
 server.use("/api/cohorts", cohortController);
 
-server.listen(3001, () => {
+server.listen(5000, () => {
 	console.log("Server now running on http://localhost:3001");
 });
--- routes/cohorts.js
+++ routes/cohorts.js
@@ -8,6 +8,16 @@ const db = knex(knexConfig);
 // Create cohort
 router.post("/", async (req, res) => {
 	try {
+		// Create cohort
+		const cohort = await db("cohorts").insert(req.body);
+
+		// Get newly created cohort
+		const newCohort = await db("cohorts")
+			.where({ id: cohort[0] })
+			.first();
+
+		// Return newly created cohort
+		res.status(201).json(newCohort);
 	} catch (err) {
 		res.status(500).json({
 			message: "Sorry, but there was an error while creating that cohort"
