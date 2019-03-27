d764bac85a0a97a7059300cce768752540779d12
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-27 13:07:59 -0700

    Connect knex in cohort route

--- routes/cohorts.js
+++ routes/cohorts.js
@@ -1,3 +1,8 @@
 const router = require("express").Router();
 
+// Knex connection
+const knex = require("knex");
+const knexConfig = require("../knexfile").development;
+const db = knex(knexConfig);
+
 module.exports = router;
