8bf150aa9fb993d13e6304e90b3aa29572dc3dcd
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-27 13:59:20 -0700

    Handle delete cohort endpoint

--- routes/cohorts.js
+++ routes/cohorts.js
@@ -87,6 +87,18 @@ router.put("/:id", async (req, res) => {
 // Delete cohort
 router.delete("/:id", async (req, res) => {
 	try {
+		const { id } = req.params;
+
+		const cohort = await db("cohorts")
+			.where({ id })
+			.del();
+
+		if (cohort < 0)
+			return res
+				.status(404)
+				.json({ message: "Sorry, but that cohort doesn't exist " });
+
+		res.status(200).json(cohort);
 	} catch (err) {
 		res.status(500).json({
 			message:
